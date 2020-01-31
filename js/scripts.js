74edfbf89704fba3b8d5df5ea00349dc093015ef
Keturah Howard <keturah.dev@gmail.com>
2020-01-31 09:36:13 -0800

    create form selectors, and achieve 2nd spec of adding size to pizza object. Create pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 // Buisness Logic---------------------------party in the back say whaaa--------------------------------------
-
+function Pizza(size) {
+  this.size = size;
+}
+// Pizza.prototype.addPizza = function (size) {
+//   var pizza = new Pizza(size);
+//   return pizza;
+// }
 
 
 // User Interface Logic -----------------------------------------------------------
@@ -7,6 +13,10 @@
 $(document).ready(function () {
   $('form#pizza-order').submit(function (event) {
     event.preventDefault();
+    var size = $('#size').val();
+
+    var pizza = new Pizza(size);
+    console.log('pizza size:', pizza.size);
     console.log('order received');
   })
 })
