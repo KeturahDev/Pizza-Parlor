b40962c1b25ef67c83a07a5909c5b5cf8391f9a5
Keturah Howard <keturah.dev@gmail.com>
2020-01-31 10:43:46 -0800

    display multiple toppings of pizza object in console

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,16 @@ function Pizza(size) {
   this.size = size;
   this.toppings = [];
 }
-Pizza.prototype.addToppings = function (pizza, toppings) {
-  // var pizzaTopping = pizza.toppings
-  pizza.toppings.push(toppings);
+Pizza.prototype.addToppings = function (toppings) {
+  this.toppings.push(toppings.sauce, toppings.cheeses[0]); //this array identifier is temorary
 }
 function Toppings(sauce) {
   this.sauce = sauce;
+  this.cheeses = []; //making this an array for the ability to add multiple types of cheese later
 }
-
+Toppings.prototype.addCheeses = function (cheese) {
+  this.cheeses.push(cheese);
+}
 
 // User Interface Logic -----------------------------------------------------------
 
@@ -20,8 +22,11 @@ $(document).ready(function () {
     var size = $('#size').val();
     var pizza = new Pizza(size);
     var sauce = $('#sauce').val();
+    var cheese = $("input:radio[name=motzerela]:checked").val();
+    console.log('cheese', cheese)
     var toppings = new Toppings(sauce);
-    pizza.addToppings(pizza, toppings);
+    toppings.addCheeses(cheese)
+    pizza.addToppings(toppings);
 
 
     console.log('pizza size:', pizza.size);
